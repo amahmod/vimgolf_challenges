6a44d4faddacf540f7ebef7580586c8f6d1d48e4
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-09-20 21:31:48 +0600

    fix: github pages deployment fails

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vimgolf_challenges/',
   plugins: [svelte()],
-  build: {
-    outDir: './docs'
-  }
 })
